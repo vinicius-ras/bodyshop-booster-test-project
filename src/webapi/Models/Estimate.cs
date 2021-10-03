9e1561e2ece5ed54880dfdb9766f4ad6463823d1
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2021-10-03 16:18:01 -0300

    Improving examples in the OpenAPI documentation for the Estimate objects.

--- src/webapi/Models/Estimate.cs
+++ src/webapi/Models/Estimate.cs
@@ -12,21 +12,26 @@ namespace BodyShopBoosterTest.Models
 		/// <summary>Unique identifier for the estimate in the database.</summary>
 		public Guid Id { get; set; }
 		/// <summary>The first name of the client who requested the estimate.</summary>
+		/// <example>John</example>
 		[Required, StringLength(maximumLength: 100, MinimumLength = 1)]
 		public string FirstName { get; set; }
 		/// <summary>The last name of the client who requested the estimate.</summary>
+		/// <example>Doe</example>
 		[Required, StringLength(maximumLength: 100, MinimumLength = 1)]
 		public string LastName { get; set; }
 		/// <summary>The type of the car for which the estimate was requested.</summary>
 		[Required, Column(TypeName = "nvarchar(20)")]
 		public VehicleType? CarType { get; set; }
 		/// <summary>Identifies the year of the vehicle for which an estimate has been requested.</summary>
+		/// <example>2014-2015</example>
 		[Required, StringLength(maximumLength: 20, MinimumLength = 1)]
 		public string Year { get; set; }
 		/// <summary>Identifies the model of the vehicle for which an estimate has been requested.</summary>
+		/// <example>Corolla</example>
 		[Required, StringLength(maximumLength: 100, MinimumLength = 1)]
 		public string Model { get; set; }
 		/// <summary>The license plate of the vehicle for which an estimate has been requested.</summary>
+		/// <example>ABCD 123</example>
 		[Required, StringLength(maximumLength: 30, MinimumLength = 1)]
 		public string LicensePlate { get; set; }
 		/// <summary>The current status of the given estimate.</summary>
